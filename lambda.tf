4e3e54487c07ae06694ef84db495466aab3e47fb
Smeet <dahiwalsmeet94@gmail.com>
2025-07-20 20:26:18 +0530

    update

--- lambda.tf
+++ lambda.tf
@@ -3,14 +3,15 @@ resource "aws_lambda_function" "lambda_function" {
   role          = aws_iam_role.lambda_exec.arn
   handler       = "index.handler"
   runtime       = "python3.9"
-  filename      = "dummy.zip" # placeholder
-  source_code_hash = filebase64sha256("dummy.zip")
+
+  s3_bucket     = "testsmeet"
+  s3_key        = "lambda-function/function.zip"
+
   publish       = true
 
   lifecycle {
     ignore_changes = [
-      filename,
-      source_code_hash
+      s3_key
     ]
   }
 }
